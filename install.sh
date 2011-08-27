ef0d3de3c42222d9574ae6ba92f606f272b54fcd
Baris Gumustas <barisgumustas@gmail.com>
2011-08-27 23:20:04 +0300

    Some sketching for installation

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# MongoDB OSX Launchctl Item
-# Author: Andrei Railean (http://andrei.md)
 if [[ `whoami` != "root" ]]; then
 echo "This script must be run as root; use sudo"
   exit
@@ -10,11 +8,13 @@ fi
 echo "Installing Bow Launcher";
 
 # copy PLIST and (re)install it
-sudo cp org.mongo.mongod.plist /System/Library/LaunchDaemons/.
-sudo chown root:wheel /System/Library/LaunchDaemons/org.mongo.mongod.plist
-sudo launchctl stop org.mongo.mongod
-sudo launchctl unload /System/Library/LaunchDaemons/org.mongo.mongod.plist
-sudo launchctl load /System/Library/LaunchDaemons/org.mongo.mongod.plist
-sudo launchctl start org.mongo.mongod
+SERVICE_NAME = "com.bow.bowd"
+PLIST_FILE = "$SERVICE_NAME.plist"
+sudo cp $PLIST_FILE /System/Library/LaunchDaemons/.
+sudo chown root:wheel /System/Library/LaunchDaemons/$PLIST_FILE
+sudo launchctl stop $SERVICE_NAME
+sudo launchctl unload /System/Library/LaunchDaemons/$PLIST_FILE
+sudo launchctl load /System/Library/LaunchDaemons/$PLIST_FILE
+sudo launchctl start $SERVICE_NAME
 
 echo "FINISHED";
